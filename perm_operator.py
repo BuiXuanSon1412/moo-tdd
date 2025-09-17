ba8163c77d8c175e3643fba2da60423bdfc8ade7
BuiXuanSon1412 <bxson1412@gmail.com>
2025-09-17 22:08:04 +0700

    add: mutation operators

--- perm_operator.py
+++ perm_operator.py
@@ -1,5 +1,7 @@
 import random
 from population import Individual
+
+# crossover
 def pmx_crossover(parent1, parent2):
     # Extract layers
     p1_layer1, p1_layer2 = parent1.chromosome[0], parent1.chromosome[1]
@@ -106,3 +108,48 @@ def cx_crossover(parent1, parent2):
 
     return (Individual([child1_layer1, child1_layer2]),
             Individual([child2_layer1, child2_layer2]))
+
+# mutation
+import random
+
+def swap_mutation(individual):
+    layer1, layer2 = individual.chromosome[0], individual.chromosome[1]
+    size = len(layer1)
+    i, j = random.sample(range(size), 2)
+    # mutate layer1
+    layer1[i], layer1[j] = layer1[j], layer1[i]
+    # optionally also mutate layer2 in same positions
+    layer2[i], layer2[j] = layer2[j], layer2[i]
+    return Individual([layer1[:], layer2[:]])
+
+
+def inversion_mutation(individual):
+    layer1, layer2 = individual.chromosome[0], individual.chromosome[1]
+    size = len(layer1)
+    i, j = sorted(random.sample(range(size), 2))
+    # reverse segment
+    layer1[i:j] = reversed(layer1[i:j])
+    layer2[i:j] = reversed(layer2[i:j])
+    return Individual([layer1[:], layer2[:]])
+
+def insertion_mutation(individual):
+    layer1, layer2 = individual.chromosome[0], individual.chromosome[1]
+    size = len(layer1)
+    i, j = random.sample(range(size), 2)
+    val1 = layer1.pop(i)
+    val2 = layer2.pop(i)
+    layer1.insert(j, val1)
+    layer2.insert(j, val2)
+    return Individual([layer1[:], layer2[:]])
+
+def scramble_mutation(individual):
+    layer1, layer2 = individual.chromosome[0], individual.chromosome[1]
+    size = len(layer1)
+    i, j = sorted(random.sample(range(size), 2))
+    # scramble inside segment
+    segment1 = layer1[i:j]
+    segment2 = layer2[i:j]
+    combined = list(zip(segment1, segment2))
+    random.shuffle(combined)
+    layer1[i:j], layer2[i:j] = zip(*combined)
+    return Individual([layer1[:], layer2[:]])
